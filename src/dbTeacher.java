64a037cd47226aecd1be7b297e3bc418d7db3e2a
Anthony Yau <antsyau@outlook.com>
2018-01-01 14:09:41 +0800

    add getOfficeHour function

--- src/dbTeacher.java
+++ src/dbTeacher.java
@@ -1,26 +1,35 @@
 import java.sql.Date;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class dbTeacher {
-    private int id;
+    private int id, timeperiod;
     private String name;
     private int upvote, downvote;
-    private ResultSet officehour;
     private ResultSet appointments;
 
     dbTeacher(int id) {
         this.id = id;
         name = getName();
-    }
-
-    void getOfficeHour() {
-        officehour = sqlCommands.sqlQuery("SELECT * FROM officehour WHERE teacherid=" + id);
+        getPeriod();
+        getAppointments();
     }
 
     void getAppointments() {
         appointments = sqlCommands.sqlQuery("SELECT * FROM appointments WHERE teacherid=" + id);
+    }
+
+    void getPeriod() {
+        ResultSet rs = sqlCommands.sqlQuery("SELECT timeperiod FROM teacher WHERE id=" + id);
+        try {
+            rs.next();
+            timeperiod = rs.getInt("timeperiod");
+        } catch (Exception e) {
+            sqlCommands.errorPrint(e);
+        }
     }
 
     int isOccupied(Date date, int timeStart, int timeEnd) {
@@ -74,12 +83,47 @@ public class dbTeacher {
         return 0;
     }
 
-    int setRegularOfficeHour(int day, int timeStart, int timeEnd, String address) {
+    int setRegularOfficeHour(int day, int timeStart, int timeEnd, String address) {  // Date; 800, 1200
         if (isOccupied(day, timeStart, timeEnd) == 0) {
             return sqlCommands.sqlUpdate("INSERT INTO officehour(teacherid,day,timeStart,timeEnd,address) VALUES (" +
                     id + "," + day + "," + timeStart + "," + timeEnd + ",\"" + address + "\")");
         }
         return 1;
+    }
+
+    int getOfficeHour(Date date) {
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        System.out.println(dayOfWeek);
+        ResultSet rs = sqlCommands.sqlQuery("SELECT * FROM officehour WHERE teacherID=" + id + " and day=" +
+                dayOfWeek + " ORDER BY `officehour`.`timeStart` ASC");
+        List<int[]> period = new ArrayList<int[]>();
+        try {
+            while (rs.next()) {
+                int timeStart = rs.getInt("timeStart");
+                int timeEnd = rs.getInt("timeEnd");
+                int hour = timeStart / 100;
+                int min = timeStart - (hour * 100);
+                while (hour * 100 + min < timeEnd) {
+                    if (min < 40) {
+                        period.add(new int[]{hour * 100 + min, hour * 100 + min + 20});
+                        min = min + 20;
+                    } else {
+                        period.add(new int[]{hour * 100 + min, (hour + 1) * 100 + min - 40});
+                        hour++;
+                        min = min - 40;
+                    }
+                }
+                int[] fin = period.get(period.size() - 1);
+                fin[1] = timeEnd;
+                period.set(period.size() - 1, fin);
+            }
+        } catch (Exception e) {
+            sqlCommands.errorPrint(e);
+            return -1;
+        }
+        return 0;
     }
 
     int delRegularOfficeHour(int day, int timeStart) {
@@ -90,7 +134,7 @@ public class dbTeacher {
         ResultSet rs = sqlCommands.sqlQuery("SELECT * FROM person WHERE id=" + id);
         try {
             rs.next();
-            return rs.getString(3);
+            return rs.getString("name");
         } catch (java.lang.NullPointerException e) {
             System.out.println("We cannot identify this teacher by Teacher ID.");
             return "error";
--- src/testClass.java
+++ src/testClass.java
@@ -1,12 +1,8 @@
 public class testClass {
     public static void main(String[] args) {
         try {
-            //ohasAuth.signUp(11717,1,"QIU Shi","LALALAND");
-            dbTeacher teacher = new dbTeacher(11717);
-            System.out.println(teacher.delRegularOfficeHour(1,800));
-            System.out.println(teacher.setRegularOfficeHour(1,800,1200,"N/A"));
-            System.out.println(teacher.setRegularOfficeHour(1,1600,2000,"N/A"));
-            System.out.println(teacher.setRegularOfficeHour(1,1100,1300,"N/A"));
+            dbTeacher Teacher = new dbTeacher(11717);
+            Teacher.getOfficeHour(java.sql.Date.valueOf("2018-1-1"));
         } catch (Exception e) {
             sqlCommands.errorPrint(e);
         }
